340eb32d9a355deb0b86324a7c7eb060b42ebbf6
Lokesh <Lokesh.K.Jangid@gmail.com>
2012-04-04 05:18:49 +0530

    PART OF LAST COMMIT. added constants and superclass of nodes.

---
+++ src/rstar/RStarNode.java
@@ -0,0 +1,18 @@
+package rstar;
+
+public class RStarNode {
+    protected long nodeId = -1;
+    protected static int _dimension;
+    protected static int CAPACITY;
+    protected MBR mbr;
+    protected long[] childPointers;   //ids of all children = filenames of children
+
+    public RStarNode() {
+    }
+
+    protected void createId() {
+        if(nodeId != -1)
+            nodeId = this.hashCode();       //create nodeId only once.
+    }
+
+}
---
+++ src/util/Constants.java
@@ -0,0 +1,20 @@
+package util;
+
+/**
+ * User: Lokesh
+ * Date: 4/4/12
+ * Time: 1:41 AM
+ */
+public class Constants {
+    public static String TREE_DATA_DIRECTORY = "data";
+    public static String TREE_FILE = "MyRStarTree.rstar";
+    public static int FILE_FORMAT_VALIDATION_KEY = 618;
+    public static String FILE_PREFIX = "rstar_";
+    public static String FILE_SUFFIX = ".node";
+
+    public static int PAGESIZE = 8*1024;
+    public static int LEAF_CAPACITY;
+    public static int INTERNAL_CAPACITY;
+    public static int DIMENSION = 2;
+}
+
